b5cc9966a6a8570bda9f50c3fdcf64f0fec2ffb8
Robin Lavallée <robin@guibec.com>
2020-10-14 10:21:26 -0500

    Linked to StreamCamel.py and scrape games now:

--- dragon.py
+++ dragon.py
@@ -16,8 +16,12 @@ import sys
 import time
 import requests
 import requests_cache
-
 import logging
+import unidecode
+
+sys.path.insert(1, os.path.abspath('./submodules/streamcamel-py'))
+from streamcamel import StreamCamel
+
 logger = logging.getLogger(__name__)
 
 def get_script_dir(follow_symlinks=True):
@@ -52,6 +56,9 @@ def get_url(url):
     if not response.from_cache:
         time.sleep(5.0)
 
+    if response.status_code >= 500:
+        logging.warning("Url: {} returned error: {}".format(url, response.status_code))
+
     content = response.content
     return content
 
@@ -64,6 +71,11 @@ def get_game_viewers(game, date):
 
     soup = BeautifulSoup(content, features="lxml")
     dom = etree.HTML(str(soup))
+
+    if dom is None:
+        logging.warning("Game {} for date {} cannot be parsed".format(game, date))
+        return -1
+
     elem = dom.xpath('/html/body/div[2]/div[2]/div[4]/div/div[3]/div/div/div[2]/div')
     if elem is not None:
         for child in elem:
@@ -71,19 +83,47 @@ def get_game_viewers(game, date):
             if is_int(value):
                 return int(value)
             else:
-                logging.warning("Game {} for date {} doesn't have a parsable viewers count: {}", game, date, child.text)
+                logging.warning("Game {} for date {} doesn't have a parsable viewers count: {}".format(game, date, child.text))
                 return -1
     else:
         return -1
+
+def scrape_game(game):
+    game_output_dir = 'output/games'
+    make_dir(game_output_dir)
+
+    sdate = datetime.date(2015, 8, 1)
+    edate = datetime.date.today()
+
+    # SullyGnome returns 0 for the current month
+    edate = edate + relativedelta(months = -1)
+
+    game_output = []
+
+    d = sdate
+    while (d < edate):
+        num_viewers = get_game_viewers(game, d)
+        print('Game {} got {} viewers for {}'.format(game, num_viewers, d))
+
+        game_entry = {}
+        game_entry['date'] = d.strftime("%Y-%m")
+        game_entry['average_viewers'] = num_viewers
+        game_output.append(game_entry)
+        d = d + relativedelta(months = 1)
+
+    with open(game_output_dir + '/' + game + '.json', 'w') as outfile:
+        json.dump(game_output, outfile, indent=4, sort_keys=True)
     
 def main(args):
     parser = argparse.ArgumentParser(description='GnomeSully Scrapping')
     parser.add_argument("--url", type=str, help="Full game URL to parse")
     parser.add_argument("--game", type=str, help="Game to parse (e.g. Fortnite)")
+    parser.add_argument("--streamcamel_games", default=False, action='store_true')
 
     args = parser.parse_args()
     url = args.url
     game = args.game
+    streamcamel_games = args.streamcamel_games
 
     make_dir('cache')
     requests_cache.install_cache('cache/cache')
@@ -97,30 +137,25 @@ def main(args):
             print(child.text)
     
     if game is not None:
-        game_output_dir = 'output/games'
-        make_dir(game_output_dir)
+        scrape_game(game)
+    
+    if streamcamel_games:
+        st = StreamCamel()
+        games = st.top_games(10)
 
-        sdate = datetime.date(2015, 8, 1)
-        edate = datetime.date.today()
+        for game in games:
+            game_name = game['name']
+            normalized_name = game_name
+            for char in ":'\"?!%$^&*":
+                normalized_name = normalized_name.replace(char, '')
 
-        # SullyGnome returns 0 for the current month
-        edate = edate + relativedelta(months = -1)
+            # Multiple spaces back to 1, and replaced by _
+            normalized_name = ' '.join(normalized_name.split())
+            normalized_name = normalized_name.replace(' ', '_')
 
-        game_output = []
-
-        d = sdate
-        while (d < edate):
-            num_viewers = get_game_viewers(game, d)
-            print('Game {} got {} viewers for {}'.format(game, num_viewers, d))
-
-            game_entry = {}
-            game_entry['date'] = d.strftime("%Y-%m")
-            game_entry['average_viewers'] = num_viewers
-            game_output.append(game_entry)
-            d = d + relativedelta(months = 1)
-
-        with open(game_output_dir + '/' + game + '.json', 'w') as outfile:
-            json.dump(game_output, outfile, indent=4, sort_keys=True)
+            normalized_name = unidecode.unidecode(normalized_name)
+            print('Game={}, normalized={}'.format(game['name'], normalized_name))
+            scrape_game(normalized_name)
 
 if __name__ == '__main__':
     logging.basicConfig(level=os.getenv('LOGLEVEL', 'INFO'), stream=sys.stdout, format='%(module)s %(message)s')
