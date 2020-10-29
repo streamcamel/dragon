86e32fdceb8e3d7ca8e9b554bc80f7454ba77787
Robin Lavallée <robin@guibec.com>
2020-10-28 21:01:00 -0500

    More custom mapping. Multiple normalized names for '_'. For 'id' for new endpoint that resulted in invalid output

--- dragon.py
+++ dragon.py
@@ -18,6 +18,7 @@ import requests
 import requests_cache
 import logging
 import unidecode
+from itertools import product
 
 sys.path.insert(1, os.path.abspath('./submodules/streamcamel-py'))
 from streamcamel import StreamCamel
@@ -66,10 +67,10 @@ def get_url(url):
 
     response = requests.get(url, headers=headers)
     if not response.from_cache:
-        time.sleep(5.0)
+        time.sleep(0.200)
 
-    if response.status_code >= 500:
-        logging.warning("Url: {} returned error: {}".format(url, response.status_code))
+    #if response.status_code >= 500:
+        #logging.warning("Url: {} returned error: {}".format(url, response.status_code))
 
     content = response.content
     return content
@@ -97,7 +98,7 @@ def get_game_information(game, date):
     dom = etree.HTML(str(soup))
 
     if dom is None:
-        logging.warning("Game {} for date {} cannot be parsed".format(game, date))
+        #logging.warning("Game {} for date {} cannot be parsed".format(game, date))
         return (None, None, None)
                                             
     average_viewers = parse_dom_value(dom, '/html/body/div[2]/div[2]/div[4]/div/div[3]/div/div/div[2]/div')
@@ -108,87 +109,107 @@ def get_game_information(game, date):
 
 
 def scrape_game(game_name, game_id=None, skip_existing=False):
-    normalized_name = normalize_name(game_name)
-    #print('Game={}, normalized={}'.format(game_name, normalized_name))
+    normalized_names = normalize_name(game_name)
 
-    game_output_dir = 'output/games'
-    make_dir(game_output_dir)
+    for normalized_name in normalized_names:
+        #print('Game={}, normalized={}'.format(game_name, normalized_name))
 
-    if skip_existing:
-        file_path = game_output_dir + '/' + normalized_name + '.json'
-        if os.path.exists(file_path):
-            with open(file_path, 'r') as infile:
-                container = json.load(infile)
-                if 'data' in container:
-                    found_error = False
-                    for d in container['data']:
-                        if 'error' in d:
-                            found_error = True
-                            break
+        game_output_dir = 'output/games'
+        make_dir(game_output_dir)
 
-                    if not found_error:
-                        return
-                
+        if skip_existing:
+            file_path = game_output_dir + '/' + normalized_name + '.json'
+            if os.path.exists(file_path):
+                with open(file_path, 'r') as infile:
+                    container = json.load(infile)
+                    if 'data' in container:
+                        found_error = False
+                        for d in container['data']:
+                            if 'error' in d:
+                                found_error = True
+                                break
 
-    sdate = datetime.date(2015, 8, 1)
-    edate = datetime.date.today()
+                        if not found_error:
+                            return
+                    
 
-    # SullyGnome returns 0 for the current month
-    edate = edate + relativedelta(months = -1)
+        sdate = datetime.date(2015, 8, 1)
+        edate = datetime.date.today()
 
-    game_output = {}
-    game_output['data'] = []
-    game_output['meta-data'] = {
-        'streamcamel_name' : game_name,
-        'sullygnome_name' : normalized_name,
-    }
+        # SullyGnome returns 0 for the current month
+        edate = edate + relativedelta(months = -1)
 
-    if not game_id is None:
-        game_output['meta-data']['game_id'] = game_id
+        game_output = {}
+        game_output['data'] = []
+        game_output['meta-data'] = {
+            'streamcamel_name' : game_name,
+            'sullygnome_name' : normalized_name,
+        }
 
-    d = sdate
-    while (d < edate):
-        (num_viewers, num_streams, peak_viewers) = get_game_information(normalized_name, d)
-        print('Game {}, viewers={}, streams={}, peak_viewers={}, for {}'.format(
-            game_name, num_viewers, num_streams, peak_viewers, d))
+        if not game_id is None:
+            game_output['meta-data']['game_id'] = game_id
 
-        game_entry = {}
-        game_entry['date'] = d.strftime("%Y-%m")
-        if num_viewers is None or num_streams is None or peak_viewers is None:
-            game_entry['error'] = 'HTML Parse Error'
-        else:
-            game_entry['average_viewers'] = num_viewers
-            game_entry['average_channels'] = num_streams
-            game_entry['peak_viewers'] = peak_viewers
+        d = sdate
+        while (d < edate):
+            (num_viewers, num_streams, peak_viewers) = get_game_information(normalized_name, d)
+            #logger.log('Game {}, viewers={}, streams={}, peak_viewers={}, for {}'.format(
+            #    game_name, num_viewers, num_streams, peak_viewers, d))
 
-        game_output['data'].append(game_entry)
-        d = d + relativedelta(months = 1)
+            game_entry = {}
+            game_entry['date'] = d.strftime("%Y-%m")
+            if num_viewers is None or num_streams is None or peak_viewers is None:
+                game_entry['error'] = 'HTML Parse Error'
+                logger.warning('Failed to fetch={} with normalized={}'.format(game_name, normalized_name))
+            else:
+                game_entry['average_viewers'] = num_viewers
+                game_entry['average_channels'] = num_streams
+                game_entry['peak_viewers'] = peak_viewers
 
-        # Early exit to investigate the issue
-        if num_viewers is None or num_streams is None or peak_viewers is None:
-            break
+            game_output['data'].append(game_entry)
+            d = d + relativedelta(months = 1)
 
-    with open(game_output_dir + '/' + normalized_name + '.json', 'w') as outfile:
-        json.dump(game_output, outfile, indent=4, sort_keys=True)
+            # Early exit to investigate the issue
+            if num_viewers is None or num_streams is None or peak_viewers is None:
+                break
+
+        with open(game_output_dir + '/' + normalized_name + '.json', 'w') as outfile:
+            json.dump(game_output, outfile, indent=4, sort_keys=True)
 
 def normalize_name(game_name):
     if game_name in mapping:
-        return mapping[game_name]
+        return [mapping[game_name]]
 
     normalized_name = game_name
     for char in ":'\"?!%$^*/\\":
         normalized_name = normalized_name.replace(char, '')
 
-    normalized_name = normalized_name.replace('+', 'plus')
-    normalized_name = normalized_name.replace('&', 'and')
+    # Gnome sometimes removes the '-' and sometimes keep it.
+    # So make up all possibily combinations
 
-    # Multiple spaces back to 1, and replaced by _
-    normalized_name = ' '.join(normalized_name.split())
-    normalized_name = normalized_name.replace(' ', '_')
+    normalized_names = []
+    key_letters = '-'
+    output_letters = ' -'
+    list_norm = list(normalized_name)
+    indices = [ i for i, c in enumerate(list_norm) if c in key_letters ]
+    for t in product(output_letters, repeat=len(indices)):
+        for i, c in zip(indices, t):
+            list_norm[i] = c
+        normalized_names.append(''.join(list_norm))
 
-    normalized_name = unidecode.unidecode(normalized_name)
+    final_names = []
+    for n in normalized_names:
+        name = n
+        name = name.replace('+', 'plus')
+        name = name.replace('&', 'and')
 
-    return normalized_name
+        # Multiple spaces back to 1, and replaced by _
+        name = ' '.join(name.split())
+        name = name.replace(' ', '_')
+
+        name = unidecode.unidecode(name)
+        final_names.append(name)
+
+    return final_names
 
 def main(args):
     read_mapping()
@@ -230,7 +251,9 @@ def main(args):
             count += 1
             game_name = game['name']
             game_id = None
-            if 'game_id' in game:
+            if 'id' in game:
+                game_id = game['id']
+            elif 'game_id' in game:
                 game_id = game['game_id']
             scrape_game(game_name, game_id=game_id, skip_existing=skip_existing)
 
@@ -245,7 +268,9 @@ def main(args):
             count += 1
             game_name = game['name']
             game_id = None
-            if 'game_id' in game:
+            if 'id' in game:
+                game_id = game['id']
+            elif 'game_id' in game:
                 game_id = game['game_id']
             scrape_game(game_name, game_id=game_id, skip_existing=skip_existing)
 
